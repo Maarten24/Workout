ab0316909052642c24b2c3f2a7a3cb5b0bd9376d
Maarten <maarten.fraune@hebk.de>
2023-01-20 15:56:40 +0100

    from old

---
+++ Stepcounter/src/StepCounter.java
@@ -0,0 +1,15 @@
+public class StepCounter {
+
+    private String date;
+    private int steps;
+
+    public StepCounter(String date) {
+        this.date = date;
+        this.steps = 0;
+    }
+
+    public incrementSteops(int steps){
+        steps++;
+    }
+
+}
